70987df9a9d77d2bdce7cac8ad3ac6397e8c38e2
Laurent Constantin <github@blackcrowsteam.com>
2014-03-15 03:11:41 +0100

    add synchronized, so score is thread safe

--- src/ch/laurent/scoreManager/ScoreStack.java
+++ src/ch/laurent/scoreManager/ScoreStack.java
@@ -12,7 +12,8 @@ import java.util.Observable;
 import java.util.Stack;
 
 /**
- * Managing score for 2 teams in a stack, so it's possible to cancel the last score.
+ * Managing score for 2 teams in a stack, so it's possible to cancel the last
+ * score.
  */
 public class ScoreStack extends Observable {
 	/**
@@ -25,8 +26,10 @@ public class ScoreStack extends Observable {
 		/**
 		 * Set score
 		 * 
-		 * @param score1 Score for team 1
-		 * @param score2 Score for team 2
+		 * @param score1
+		 *            Score for team 1
+		 * @param score2
+		 *            Score for team 2
 		 */
 		Score(final int score1, final int score2) {
 			this.score1 = score1;
@@ -36,7 +39,8 @@ public class ScoreStack extends Observable {
 		/**
 		 * Get score for a team
 		 * 
-		 * @param teamId Team id (1,2)
+		 * @param teamId
+		 *            Team id (1,2)
 		 * @return score
 		 */
 		public int get(final int teamId) {
@@ -50,8 +54,7 @@ public class ScoreStack extends Observable {
 	private final Stack<Score> stack = new Stack<Score>();
 
 	/**
-	 * Add score.
-	 * Negative score are remplaced by 0.
+	 * Add score. Negative score are remplaced by 0.
 	 */
 	private Score pushQuiet(final Score s) {
 		// no negative score
@@ -61,12 +64,15 @@ public class ScoreStack extends Observable {
 		if (s.score2 < 0)
 			s.score2 = 0;
 
-		return stack.push(s);
+		synchronized (stack) {
+			return stack.push(s);
+		}
 	}
-	
-	private Score pushQuiet(final int score1, final int score2){
-        return pushQuiet(new Score(score1, score2));
+
+	private Score pushQuiet(final int score1, final int score2) {
+		return pushQuiet(new Score(score1, score2));
 	}
+
 	/**
 	 * Starting score is (0,0)
 	 */
@@ -78,11 +84,15 @@ public class ScoreStack extends Observable {
 	/**
 	 * Clear all previous value and set a new one
 	 * 
-	 * @param score1 Score for team 1
-	 * @param score2 Score for team 2
+	 * @param score1
+	 *            Score for team 1
+	 * @param score2
+	 *            Score for team 2
 	 */
 	private void setAndNotify(final int score1, final int score2) {
-		stack.clear();
+		synchronized (stack) {
+			stack.clear();	
+		}
 		this.pushQuiet(new Score(score1, score2));
 		notifyObservers();
 
@@ -91,20 +101,27 @@ public class ScoreStack extends Observable {
 	/**
 	 * Add the previous score with the new one and insert it into the stack.
 	 * 
-	 * @param score1 Score for team 1
-	 * @param score2 Score for team 2
+	 * @param score1
+	 *            Score for team 1
+	 * @param score2
+	 *            Score for team 2
 	 */
 	public void addAndNotify(final int score1, final int score2) {
-		Score s = (stack.isEmpty() ? new Score(0, 0) : stack.peek());
-		this.pushQuiet(new Score(s.score1 + score1, s.score2 + score2));
+		synchronized (stack) {
+			Score s = (stack.isEmpty() ? new Score(0, 0) : stack.peek());	
+			this.pushQuiet(new Score(s.score1 + score1, s.score2 + score2));
+			
+		}
 		notifyObservers();
 	}
 
 	/**
 	 * Add an anounce
 	 * 
-	 * @param team Team id
-	 * @param announce Value
+	 * @param team
+	 *            Team id
+	 * @param announce
+	 *            Value
 	 */
 	public void addAnnounce(final int team, final int announce) {
 		int score1 = (team == 1 ? announce : 0);
@@ -116,12 +133,15 @@ public class ScoreStack extends Observable {
 	/**
 	 * Get score for the specified team
 	 * 
-	 * @param team Team id
+	 * @param team
+	 *            Team id
 	 * @return Score
 	 */
 	public int getScore(final int team) {
 		// stack is never empty.
-		return stack.peek().get(team);
+		synchronized (stack) {
+			return stack.peek().get(team);
+		}
 	}
 
 	/**
@@ -136,24 +156,27 @@ public class ScoreStack extends Observable {
 	 * Cancel the last score
 	 */
 	public void cancel() {
-		// Remove from stack
-		if (!stack.empty())
-			stack.pop();
+		synchronized (stack) {
 
-		// Be sure the stack is never empty
-		if (stack.empty()) {
-			// notifyObservers() will be called
-			setAndNotify(0, 0);
-		} else {
-			// We notify in this case..
-			notifyObservers();
+			// Remove from stack
+			if (!stack.empty())
+				stack.pop();
+
+			// Be sure the stack is never empty
+			if (stack.empty()) {
+				// notifyObservers() will be called
+				setAndNotify(0, 0);
+			} else {
+				// We notify in this case..
+				notifyObservers();
+			}
 		}
 	}
-	
+
 	/**
 	 * Notify observer with an implicit setChanged.
 	 */
-	public void notifyObservers(){
+	public void notifyObservers() {
 		setChanged();
 		super.notifyObservers();
 	}
@@ -165,26 +188,29 @@ public class ScoreStack extends Observable {
 	 */
 	public boolean isCancellable() {
 		// La pile contient toujours une valeur (0,0)
-		return stack.size() > 1;
+		synchronized (stack) {
+			return stack.size() > 1;
+		}
 	}
 
 	/**
-	 * Saving stack into a string
-	 * Stack = Score ; Score ; Score
-     * Score = score1,score2
-     * 
+	 * Saving stack into a string Stack = Score ; Score ; Score Score =
+	 * score1,score2
+	 * 
 	 * @return a string
 	 */
 	public String saveAsString() {
-		// Stack = Score ; Score ; Score
-		// Score = score1,score2
 		StringBuffer s = new StringBuffer();
-		final int size = stack.size();
-		for (int i = 0; i < size; i++) {
-			Score score = stack.get(i);
-			s.append(score.score1 + "," + score.score2);
-			if (i != size - 1) {
-				s.append(";");
+		synchronized (stack) {
+			// Stack = Score ; Score ; Score
+			// Score = score1,score2
+			final int size = stack.size();
+			for (int i = 0; i < size; i++) {
+				Score score = stack.get(i);
+				s.append(score.score1 + "," + score.score2);
+				if (i != size - 1) {
+					s.append(";");
+				}
 			}
 		}
 		return s.toString();
@@ -196,20 +222,27 @@ public class ScoreStack extends Observable {
 	 * @return the stack
 	 */
 	public Stack<ScoreStack.Score> getStack() {
-		return stack;
+		synchronized (stack) {
+			return stack;
+		}
 	}
 
 	/**
-	 * Build stack from string
-	 * Stack = Score ; Score ; Score
-     * Score = score1,score2
-	 * @param s string
+	 * Build stack from string Stack = Score ; Score ; Score Score =
+	 * score1,score2
+	 * 
+	 * @param s
+	 *            string
 	 */
 	public void restoreFromString(String s) {
-		if (s == null || s.trim().equals("")) return;
-		// Stack = Score ; Score ; Score
+		if (s == null || s.trim().equals(""))
+			return;
 		String tmpScores[] = s.split(";");
-		stack.clear();
+
+		synchronized (stack) {
+			stack.clear();
+		}
+
 		String[] score;
 		for (int i = 0; i < tmpScores.length; i++) {
 			score = tmpScores[i].split(",");
